1a33e3977078271c60f6c043ea7674de4c05da43
XisHungry <60353325+XisHungry@users.noreply.github.com>
2020-01-28 00:10:08 +0700

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -1,44 +1,11 @@
-/*
- * Copyright (c) 2014, RoboPeak
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, 
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/*
- *  RoboPeak LIDAR System
- *  RPlidar ROS Node client test app
- *
- *  Copyright 2009 - 2014 RoboPeak Team
- *  http://www.robopeak.com
- * 
- */
-
-
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "iostream"
+#include "fstream"
 
 #define RAD2DEG(x) ((x)*180./M_PI)
+
+using namespace std;
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
@@ -46,20 +13,84 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
     ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
     ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
   
-    for(int i = 0; i < count; i++) {
+    for(int i = 0; i < count; i++) 
+    {
         float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
         ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
+
+	int Frontfile, Backfile, Leftfile, Rightfile;
+	double F, B, L, R;
+	
+	if (degree > 178 && degree < 180)
+	{
+		ofstream Frontfile;
+		Frontfile.open ("Front.txt");
+		F = scan->ranges[i];
+		if (F >= 0 && F <= 6)
+		{
+			Frontfile << F;
+		  	Frontfile.close();
+		}
+		else
+		{
+			Frontfile << 6.0;
+		}
+	}
+
+	if (degree > 0 && degree < 2)
+	{
+	ofstream Backfile;
+	Backfile.open ("Back.txt");
+	B = scan->ranges[i];
+ 		if (B >= 0 && B <= 6)
+		{
+			Backfile << B;
+		  	Backfile.close();
+		}
+		else
+		{
+			Backfile << 6.0;
+		}
+	}
+
+	if (degree > -95  && degree < -93)
+	{
+	ofstream Leftfile;
+	Leftfile.open ("Left.txt");
+	L = scan->ranges[i];
+		if (L >= 0 && L <= 6)
+		{
+			Leftfile << L;
+		  	Leftfile.close();
+		}
+		else
+		{
+			Leftfile << 6.0;
+		}
+	}
+
+	if (degree > 93  && degree < 95)
+	{
+	ofstream Rightfile;
+	Rightfile.open ("Right.txt");
+	R = scan->ranges[i];
+		if (R >= 0 && R <= 6)
+		{
+			Rightfile << R;
+		  	Rightfile.close();
+		}
+		else
+		{
+			Rightfile << 6.0;
+		}
+	}
     }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "rplidar_node_client");
     ros::NodeHandle n;
-
     ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
-
     ros::spin();
-
-    return 0;
 }
